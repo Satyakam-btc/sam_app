422d0d13f575ce107ea8f15100ae615a60b4c0e4
Satyakam-btc <satyakam.parikh@botreeconsulting.com>
2014-08-21 12:53:55 +0530

    finished user edit, update inde

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,13 +3,20 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
+  
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def new
     @user = User.new
   end
 
-def create
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
     @user = User.new(user_params)
     if @user.save
       sign_in @user
@@ -20,6 +27,10 @@ def create
     end
   end
 
+  def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
+
   private
 
     def user_params
@@ -27,7 +38,40 @@ def create
                                    :password_confirmation)
     end
   def destroy
-    sign_out
-    redirect_to root_url
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
 end
