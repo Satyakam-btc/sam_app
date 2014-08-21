56f2fc549f43bf7b3e9bfe47e81b67ce5d6dad7a
Satyakam-btc <satyakam.parikh@botreeconsulting.com>
2014-08-21 10:53:23 +0530

    sign in done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
 def create
     @user = User.new(user_params)
     if @user.save
+      sign_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
@@ -25,4 +26,8 @@ def create
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+  def destroy
+    sign_out
+    redirect_to root_url
+  end
 end
